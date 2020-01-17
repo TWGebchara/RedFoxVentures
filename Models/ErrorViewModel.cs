01337a0785415ef417608a6b55cff49c8cac37a9
Tamerat Gebchara <tgebchara@gmail.com>
2020-01-17 18:36:25 +0000

    Commiting RedFoxVentures

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedFoxVentures.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
